4a404878e862d3dd4a290cd44dd3ffc786bc5aa3
vandang0104 <makid1523@gmail.com>
2025-08-08 14:24:24 +0700

    Tạo thêm thư mục controller

--- routes/client/home.route.js
+++ routes/client/home.route.js
@@ -1,8 +1,7 @@
 const express = require("express") ;
 const router = express.Router() ;
+const controller = require("../../controllers/client/home.controller")
 
-router.get('/',(req,res)=>{
-  res.render("client/pages/home/index") ;
-});
+router.get('/',controller.index);
 
 module.exports = router ;
--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -1,7 +1,7 @@
 const express = require("express") ;
 const router = express.Router() ;
-router.get('/',(req,res)=>{
-  res.render("client/pages/products/index") ;
-});
+const controller = require("../../controllers/client/product.controller")
+
+router.get('/',controller.index);
 
 module.exports = router ;
