6a2b1049995f1107857324210498a4955c5d6523
vandang0104 <makid1523@gmail.com>
2025-08-08 21:18:23 +0700

    Làm trang danh sách sản phẩm

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,18 @@
-module.exports.index= (req,res)=>{
+const Product = require("../../models/product.model") ;
+
+module.exports.index= async (req,res)=>{
+  const products = await Product.find({
+    status: "active",
+    deleted: false
+  }) ;
+
+  const newProducts=products.map(item => {
+    item.priceNew = (item.price*(100-item.discountPercentage)/100).toFixed(2) ;
+    return item ;
+  })
+  console.log(products) ;
   res.render("client/pages/products/index",{
-    pageTitle: "Danh sách sản phẩm"
+    pageTitle: "Danh sách sản phẩm",
+    products: newProducts
   }) ;
 }
