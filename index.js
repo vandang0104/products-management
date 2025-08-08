ec93821781cb30c169fab0bab0509cc177370442
vandang0104 <makid1523@gmail.com>
2025-08-08 15:02:50 +0700

    Cấu trúc lại folder PUG

--- index.js
+++ index.js
@@ -10,14 +10,6 @@ app.set("views", "./views");
 app.set("view engine", "pug");
 
 route(app);
-// app.get('/',(req,res)=>{
-//     res.render("client/pages/home/index") ;
-// });
-
-// app.get('/products',(req,res)=>{
-//     res.render("client/pages/products/index") ;
-
-// });
 
 app.listen(port,()=>{
     console.log(`App listener port ${port}`)
