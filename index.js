ea57228ccc3e5ead59ed15e1654174bab8bf3621
vandang0104 <makid1523@gmail.com>
2025-08-11 20:09:08 +0700

    Xây dựng layout chung

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express") ;
 require("dotenv").config() ;
 
+const systemConfig =require("./config/system") ;
+
 const route = require('./routes/client/index.route');
 const routeAdmin = require('./routes/admin/index.route')
 const database = require("./config/database") ;
@@ -10,10 +12,11 @@ database.connect() ;
 const app = express() ;
 const port = process.env.PORT ;
 
-
 app.use(express.static("public"));
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+app.locals.prefixAdmin = systemConfig.prefixAdmin ;
 
 route(app);
 routeAdmin(app) ;
