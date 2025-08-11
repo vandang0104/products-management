ae05c116add61fc19d8e93fa996cfe52f2843b22
vandang0104 <makid1523@gmail.com>
2025-08-11 21:49:28 +0700

    Lấy data in ra giao diện

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -3,7 +3,11 @@
 const Product = require("../../models/product.model") ;
 
 module.exports.index = async (req,res)=>{
-  const products = await Product.find({}) ;
+  const products = await Product.find({
+    deleted: false 
+  }) ;
+
+  console.log(products) ;
   
   res.render("admin/pages/product/index",{
     pageTitle: "Danh sách sản phẩm" ,
