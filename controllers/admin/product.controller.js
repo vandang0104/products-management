1310d6cefc2107a17816727d9ce25fb43deb6612
vandang0104 <makid1523@gmail.com>
2025-08-13 10:24:14 +0700

    Tối ưu phần bộ lọc

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,33 +1,10 @@
 // [GET] /admin/products
 
 const Product = require("../../models/product.model") ;
+const filterStatusHelper = require("../../helpers/filterStatus") ;
 
 module.exports.index = async (req,res)=>{
-
-  let filterStatus =[
-    {
-      name:"Tất cả" ,
-      status:"" ,
-      class:""
-    },
-    {
-      name:"Hoạt động" ,
-      status:"active" ,
-      class:""
-    },
-    {
-      name:"Dừng hoạt động" ,
-      status:"inactive" ,
-      class:""
-    }
-  ]
-  if(req.query.status){
-    const index = filterStatus.findIndex(item => item.status == req.query.status) 
-    filterStatus[index].class = "active" ;
-  } else {
-    const index = filterStatus.findIndex(item => item.status == "") 
-    filterStatus[index].class = "active" ;
-  }
+  const filterStatus = filterStatusHelper(req.query) ; 
 
   let find = {
     deleted: false 
@@ -42,7 +19,6 @@ module.exports.index = async (req,res)=>{
   }
 
   if(req.query.status){
-    
     find.status = req.query.status ;
   }
 
