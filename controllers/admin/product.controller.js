482b35a3cdbf4afa73f6297ae513de994b9415b8
vandang0104 <makid1523@gmail.com>
2025-08-19 09:56:14 +0700

    Xóa mềm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -76,7 +76,11 @@ module.exports.changeMulti = async (req,res) => {
 // [DELETE] /admin/products/delete/:id
 module.exports.deleteItem = async (req,res) => {
   const id = req.params.id ;
-  await Product.deleteOne({_id:id}) ;
+  // await Product.deleteOne({_id:id}) ;
+  await Product.updateOne({_id:id} ,
+    {deleted:true ,
+    deletedAt: new Date()
+  }) ;
   const redirectUrl = req.get("Referer") || "/admin/products";
   res.redirect(redirectUrl);
 }
