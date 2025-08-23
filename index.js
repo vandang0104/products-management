22f2de599af3960d66a4da2f92b1f54e95c9cee1
vandang0104 <makid1523@gmail.com>
2025-08-23 13:03:51 +0700

    setting to deplot

--- index.js
+++ index.js
@@ -29,10 +29,11 @@ app.use(session({ cookie: { maxAge: 60000 } }));
 app.use(flash());
 // END Flash
 
-app.use(express.static("public"));
-app.set("views", "./views");
-app.set("view engine", "pug");
+app.use(express.static(`${__dirname}/public`));
 
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "pug");
+ 
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 route(app);
